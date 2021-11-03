e979c4a653a84a6dfe271e0604ed1b6c2750ace4
Xvegetta <72033570+Xvegetta@users.noreply.github.com>
2021-11-03 14:46:30 -0600

    juego con boton

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="es-MX">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pregunta</title>
+</head>
+<body>
+    <style>
+        body{
+            background-color: darkgrey;
+        }
+        .btn{
+            background-color: cadetblue;
+            color: white;
+            font-family: 'Courier New', Courier, monospace;
+            font-size: 1.2em;
+            border-radius: 5px;
+            position: absolute;
+            cursor: pointer;
+            border-radius: 5px;
+        }
+        #btn_no{
+            margin: 2em;
+            left: 6%;
+            top: 8.3%;
+        }
+        #btn_si{
+            margin: 1em;
+        }
+    </style>
+    <h1>¿Quieres ser mi novia?</h1>
+    <button class="btn" id="btn_si">SI</button>
+    <button class="btn" id="btn_no">NO</button>
+</body>
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script >
+    $("#btn_no").hover(function () {
+    $("#botonQueSeMueve").css("top", `${Math.random() * 710}px`);
+    $("#botonQueSeMueve").css("left", `${Math.random() * 1000}px`);
+    });
+</script>
+</html>
